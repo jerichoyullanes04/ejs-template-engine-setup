a50eec36a3fb596ec5d011ba658f9be4f4062e1c
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-06 01:21:35 +0800

    Added Data to Views

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'My Website',
+        message: 'Hello from EJS',
+    });
 });
 
 app.listen(8000, () => console.log('Server started'));
